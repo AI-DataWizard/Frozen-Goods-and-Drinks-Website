5eae16e0d1a3db027b6104f01e4484515aab04e9
AI-DataWizard <tadiwachops3567@gmail.com>
2025-04-08 18:51:14 +0200

    first commit

---
+++ code/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+  $("#menu-btn").click(function () {
+    $(".navbar").toggleClass("active");
+  });
+
+  $("#login-btn").click(function () {
+    $(".login-form").addClass("active");
+  });
+
+  $("#close-login-form").click(function () {
+    $(".login-form").removeClass("active");
+  });
+
+  $(window).on("scroll load", function () {
+    if (scrollY > 0) {
+      $(".header").addClass("active");
+    } else if (scrollY >= 0) {
+      $(".header").removeClass("active");
+    }
+  });
+
+  $(".menu-slider").owlCarousel({
+    loop: true,
+    nav: true,
+    items: 3,
+    autoplay: true,
+    center: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      650: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+  $(".review-slider").owlCarousel({
+    loop: true,
+    nav: true,
+    items: 3,
+    autoplay: true,
+    center: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      650: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+});
